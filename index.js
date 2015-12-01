89f9241231e16f674b9fb8805cd86c4adf4be6b5
wuhy <sparklewhy@gmail.com>
2015-12-01 20:27:28 +0800

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+/**
+ * @file 入口模块
+ * @author sparklewhy@gmail.com
+ */
+
+var pkgManage = require('fisx-package');
+
+exports.name = 'list';
+exports.desc = 'list installed component packages';
+exports.options = {
+    '-h, --help': 'print this help message',
+    '-r, --root <path>': 'set project root',
+    '-u, --update': 'force install latest version on conflict'
+};
+
+exports.run = function (argv, cli, env) {
+    if (argv.h || argv.help) {
+        return cli.help(exports.name, exports.options);
+    }
+
+    var options = {
+        root: env.cwd,
+        availableUpdate: argv.update || argv.u
+    };
+    return pkgManage.initProjectRoot(env.configNameSearch[0], options, fis)
+        .then(pkgManage.loadUserConfig.bind(this, env.configNameSearch[0], options, fis))
+        .then(function () {
+            return pkgManage.list(options);
+        });
+};
